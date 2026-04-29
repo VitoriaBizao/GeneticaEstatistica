28d4cf65126036c722417187cb6519e62e8f8bcd
PedroHVargas <pedrohvargas@usp.br>
2026-04-29 08:38:23 -0300

    update

--- Malawi/Trabalho Final/Scripts/AnaliseExploratória.R
+++ Malawi/Trabalho Final/Scripts/AnaliseExploratória.R
@@ -1,4 +1,31 @@
 library(tidyverse)
+
 data <- read.csv('https://raw.githubusercontent.com/mauricioaraujj/Pan_African_Trials_Network/refs/heads/main/data/data.csv', sep = ';')
-datsub <- data |> subset(data$COUNTRY == "Malawi")
-unique(datsub$env) 
+
+malawi <- data |> subset(data$COUNTRY == "Malawi")
+
+# contando ambientes
+unique(malawi$env) %>% length()
+
+# comparador
+env_malawi <- unique(malawi$env)
+
+comparador <- function(dataset, nome_dataset) {
+  correspondencia <- sum(env_malawi %in% dataset$env)
+  total <- length(env_malawi)
+  
+  print(paste(nome_dataset, ":", correspondencia, "/", total))
+}
+
+# datasets
+tabelas <- list(
+  weather = weather,
+  Elevation = Elevation,
+  bioclimatic = bioclimatic,
+  coords = coords,
+  soil = soil
+)
+
+for (nome in names(tabelas)) {
+  comparador(tabelas[[nome]], nome)
+}
